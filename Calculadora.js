e520f75b14d623f4799cf2b16548499f70255c94
Novato117 <luisfernandojuarez117@outlook.com>
2022-12-19 20:13:31 -0600

    first commit

---
+++ Calculadora.js
@@ -0,0 +1,43 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+    raizCuadrada(n1) {
+        return Math.sqrt(n1);
+    }
+    potencia(n1, n2) {
+        return Math.pow(n1, n2);
+    }
+}
+/*class Calculadora {
+    sumar(n1, n2) {
+        return n1 + n2;
+    }
+    restar(n1, n2) {
+        return n1 - n2;
+    }
+    dividir(n1, n2) {
+        return n1 / n2;
+    }
+    multiplicar(n1, n2) {
+        return n1 * n2;
+    }
+    raizCuadrada(n1) {
+        return Math.sqrt(n1);
+    }
+    potencia(n1) {
+        return Math.pow(n1, 2);
+    }
+}*/
